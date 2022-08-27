c5599a83fada0e14accefcbe3afeb12977be211b
Chad Oakenfold <chad@oakenfold.ca>
2022-08-27 11:39:21 -0400

    Force actions to build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { AppProviders } from "./AppProviders";
 import { SineCoordinates } from "./SineCoordinates/SineCoordinates";
-
+console.log("FE", process.env);
 export const App = () => {
   return (
     <AppProviders>
